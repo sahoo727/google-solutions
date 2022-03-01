814134ed54dd89a981b73ceef55eff3029e7374c
sahoo727 <soubhagyasahoo727@gmail.com>
2022-03-01 21:03:10 +0530

    added partner

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import Landing from './Components/Landing';
 import './App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Partner from './Components/landing/Partner';
 
 function App() {
   return (
     <div className="App">
       <Landing/>
+      <Partner/>
     </div>
   );
 }
---
+++ client/src/Components/landing/Partner.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import First from './images/deskyop.png'
+import Second from './images/Screenshot 2021-12-06 163856.png'
+import Third from './images/Profile.jpg'
+import './Partner.css'
+
+export default class Partner extends Component {
+  render() {
+    var settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      pauseOnHover: true,
+      
+      
+    };
+    return (
+      <div>
+        <h2>Our Partners</h2>
+        <Slider {...settings}>
+          
+            <img src={Second} />
+            <img src={Second}/>
+            <img src={Third}/>
+            <img src={Second}/>
+            <img src={Second}/>
+            <img src={Third}/>
+          
+        </Slider>
+      </div>
+    );
+  }
+}
